881c51c0420843b47ff2a5310ca297bce8d261d7
Isabel Anastasiadis <isabel@boost.co.nz>
2022-11-11 10:52:30 +1300

    feat: initial gem

---
+++ lib/codeclimate_diff.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "codeclimate_diff/version"
+
+module CodeclimateDiff
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/codeclimate_diff/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module CodeclimateDiff
+  VERSION = "0.1.0"
+end

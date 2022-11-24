c7e26a7194876a0b4a917a980a9ef5777f6d3c6f
Isabel Anastasiadis <isabel@boost.co.nz>
2022-11-25 11:59:46 +1300

    fix: config structure changed

--- lib/codeclimate_diff/runner.rb
+++ lib/codeclimate_diff/runner.rb
@@ -11,7 +11,7 @@ module CodeclimateDiff
   class Runner
     def self.calculate_changed_filenames(pattern)
       extra_grep_filter = pattern ? " | grep '#{pattern}'" : ""
-      branch_name = CodeclimateDiff.configuration["gitlab"]["main_branch_name"]
+      branch_name = CodeclimateDiff.configuration["main_branch_name"]
       files_changed_str = `git diff --name-only #{branch_name} | grep --invert-match spec/ | grep --extended-regexp '.js$|.rb$'#{extra_grep_filter}`
       puts "Files changed on branch: #{files_changed_str}"
 
